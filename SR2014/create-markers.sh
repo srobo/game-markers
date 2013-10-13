4a9ada6ec21fd4a33303a157270ea3184a9415e4
Peter Law <PeterJCLaw@gmail.com>
2013-10-13 21:51:42 +0100

    Add (disabled) support for generating arena and robot markers.
    This is to show how it would be done, but is disabled as it's not
    what was used. This is included as a reference only.
    
    Change-Id: Ife3c9e2c1e932b49ab5f5b115004188aa342a17f

--- SR2014/create-markers.sh
+++ SR2014/create-markers.sh
@@ -4,6 +4,22 @@
 BASE=0
 koki_marker_gen=./kokimarker/koki-marker-gen
 
+if [ 1 == 2 ]
+then
+    echo bees
+    for i in `seq 0 27`
+    do
+        id=$((i + BASE))
+        $koki_marker_gen --width 250 --paper-size A3 --desc "ARENA $i" $id arena
+    done
+
+    for i in `seq 0 3`
+    do
+        id=$((i + BASE + 28))
+        $koki_marker_gen --width 100 --desc "ROBOT $i" $id robot
+    done
+fi
+
 for i in `seq 0 7`
 do
     id=$((i + BASE + 32))
