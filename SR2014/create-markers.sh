ca167c7214192cc831f71f679eebe5cad0132d31
Peter Law <PeterJCLaw@gmail.com>
2013-10-13 21:50:20 +0100

    Generate 2014 markers.
    Modify the script to use the now submoduled kokimarker, add the
    the generated markers for slots & tokens. Note that, as I did in
    srweb, the arena & robot tokens are simply copied from last year
    as they haven't changed.
    
    Change-Id: Ide011e220317f1cce1ac86a5a45c20830ee4034b

--- SR2014/create-markers.sh
+++ SR2014/create-markers.sh
@@ -2,27 +2,28 @@
 
 # set to 100 to generate the competition markers.
 BASE=0
+koki_marker_gen=./kokimarker/koki-marker-gen
 
 for i in `seq 0 7`
 do
     id=$((i + BASE + 32))
-    ./koki-marker-gen --width 160 --desc "SLOT $i" $id slot
+    $koki_marker_gen --width 160 --desc "SLOT $i" $id slot
 done
 
 for i in `seq 0 3`
 do
     id=$((i + BASE + 40))
-    ./koki-marker-gen --width 160 --desc "TOKEN TOP $i" $id token-top
+    $koki_marker_gen --width 160 --desc "TOKEN TOP $i" $id token-top
 done
 
 for i in `seq 0 3`
 do
     id=$((i + BASE + 44))
-    ./koki-marker-gen --width 160 --desc "TOKEN BOTTOM $i" $id token-bottom
+    $koki_marker_gen --width 160 --desc "TOKEN BOTTOM $i" $id token-bottom
 done
 
 for i in `seq 0 3`
 do
     id=$((i + BASE + 48))
-    ./koki-marker-gen --width 160 --desc "TOKEN SIDE $i" $id token-side
+    $koki_marker_gen --width 160 --desc "TOKEN SIDE $i" $id token-side
 done
