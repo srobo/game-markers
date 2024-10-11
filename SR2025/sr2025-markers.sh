9b43abdcfc711cad174bde518df31aa4da719de9
Will Barber <WillB97@users.noreply.github.com>
2024-10-11 21:45:07 +0100

    Be pendantic
    Co-authored-by: Jake Howard <RealOrangeOne@users.noreply.github.com>

--- SR2025/sr2025-markers.sh
+++ SR2025/sr2025-markers.sh
@@ -40,17 +40,17 @@ april_vision marker_generator TILE \
     --left_margin 10 --top_margin 10 --row_padding 20 --column_padding 20 \
     --marker_size 80 --range 160-179 \
     --description_format "Student Robotics - Yellow Pallet - #{marker_id}" --all_filename "yellow_pallets_a3.pdf"
-# Hi-rises
+# High-rises
 april_vision marker_generator SINGLE \
     --marker_size 80 --range 195-199 \
-    --description_format "Student Robotics - Hi-rise - #{marker_id}" --all_filename "hirise_markers.pdf"
+    --description_format "Student Robotics - High-rise - #{marker_id}" --all_filename "highrise_markers.pdf"
 april_vision marker_generator TILE \
     --num_rows 2 --num_columns 2 --repeat 4 --page_size A3 \
     --left_margin 20 --top_margin 20 --row_padding 40 --column_padding 40 \
     --marker_size 80 --range 195-199 \
-    --description_format "Student Robotics - Hi-rise - #{marker_id}" --all_filename "hirise_markers_a3.pdf"
+    --description_format "Student Robotics - High-rise - #{marker_id}" --all_filename "highrise_markers_a3.pdf"
 
 echo "Collate all the non-A3 marker sheets into a single PDF"
-# pdfunite arena_markers.pdf green_pallets.pdf orange_pallets.pdf magenta_pallets.pdf yellow_pallets.pdf hirise_markers.pdf SR2025_markers_a4.pdf
+# pdfunite arena_markers.pdf green_pallets.pdf orange_pallets.pdf magenta_pallets.pdf yellow_pallets.pdf highrise_markers.pdf SR2025_markers_a4.pdf
 echo "Collate the A3 marker sheets with the arena markers into a single PDF"
-# pdfunite arena_markers.pdf green_pallets_a3.pdf orange_pallets_a3.pdf magenta_pallets_a3.pdf yellow_pallets_a3.pdf hirise_markers_a3.pdf SR2025_markers.pdf
+# pdfunite arena_markers.pdf green_pallets_a3.pdf orange_pallets_a3.pdf magenta_pallets_a3.pdf yellow_pallets_a3.pdf highrise_markers_a3.pdf SR2025_markers.pdf
